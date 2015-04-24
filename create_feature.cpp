917e46df745612a679ccfe9797ed29e3f1ed5d62
XingKaihang <358141721@qq.com>
2015-04-24 04:13:06 -0700

    1

--- create_feature.cpp
+++ create_feature.cpp
@@ -19,11 +19,28 @@
 using namespace cv;
 using namespace std;
 
+void save(const string &file_name,cv::PCA pca_)
+{
+    FileStorage fs(file_name,FileStorage::WRITE);
+    fs << "mean" << pca_.mean;
+    fs << "e_vectors" << pca_.eigenvectors;
+    fs << "e_values" << pca_.eigenvalues;
+    fs.release();
+}
+
+int load(const string &file_name,cv::PCA pca_)
+{
+    FileStorage fs(file_name,FileStorage::READ);
+    fs["mean"] >> pca_.mean ;
+    fs["e_vectors"] >> pca_.eigenvectors ;
+    fs["e_values"] >> pca_.eigenvalues ;
+    fs.release();
+
+}
 
 int main( )
 {
-    int nTrainFaces         = 0;    // number of trainning images
-    int nEigens             = 100;    // number of eigenvalues
+    int nEigens             = 50;    // number of eigenvalues
     PCA* pca_analysis = NULL;
 
 //load faces
@@ -40,7 +57,10 @@ int main( )
     
     // count the number of faces
     while( fgets(imgFilename, 512, imgListFile) ) 
+    {
         ++ nFaces;
+    }    
+    cout<<"**********face number:"<<nFaces<<"*****************\n";
     rewind(imgListFile);
 
     // allocate the face-image array and person number matrix
@@ -53,7 +73,7 @@ int main( )
     {
         //read person number and name of image file
         fscanf(imgListFile, "%s %s", name_array[iFace], imgFilename);
-
+        //cout<<name_array[iFace]<<"  "<<imgFilename<<"\n";
         // load the face image
         faceImgArr[iFace] = imread(imgFilename,CV_LOAD_IMAGE_GRAYSCALE);
     }
@@ -64,46 +84,58 @@ int main( )
 //start PCA
     int size = faceImgArr[0].cols * faceImgArr[0].rows;
     Mat mat(size, nFaces, CV_8UC1);
+
+
     int row,col;
+    Mat col_tmp ;
     for(i = 0; i < nFaces; i++)
     {
-        Mat col_tmp ;
         faceImgArr[i].reshape(1, size).col(0).convertTo(col_tmp, CV_8UC1,1);
-        for(row = 0; row < size; row++)
-        {
-            *(mat.data + mat.step[0] * row + mat.step[1] * i) = (int)(*(col_tmp.data + col_tmp.step[0] * row + col_tmp.step[1] * i));
-        }
+        col_tmp.col(0).copyTo(mat.col(i));
+        // for(row = 0; row < size; row++)
+        // {
+        //     *(mat.data + mat.step[0] * row + mat.step[1] * i) = (int)(*(col_tmp.data + col_tmp.step[0] * row + col_tmp.step[1] * i));
+        // }
     }
+
     pca_analysis = new PCA( mat,Mat(),CV_PCA_DATA_AS_COL,nEigens);
     Mat vector = (*pca_analysis).project(mat);
 
-//end PCA
+   
+    // Mat single = (*pca_analysis).project(col_tmp);
+    // cout<<"original:"<<single.size();
+    // cout<<single;
 
-//start output PCA
-    int vector_rows = vector.rows;
+
+
+    //start output person identify
+    int vector_cols = vector.cols;
 
     ofstream f1("feature.txt");
 
-    f1<<"every row represents a person's feature( "<<vector_rows<<" cols)\n";
-    f1<<"name f_1 f_2 f_3 ......f_"<<vector_rows;
+    f1<<vector_cols;
 
     Mat temp_mat;
-    for(row = 0;row < vector_rows; row++)
+    for(row = 0;row < nFaces; row++)
     {
-        f1<<"\n";
-        f1<<name_array[row]<<"";
+
+        f1<<name_array[row]<<",";
         
         temp_mat = vector.row(row);
-        for(col = 0; col<vector_rows;col++)
+        for(col = 0; col<vector_cols;col++)
         {
-            f1<<" "<<(int)*(temp_mat.data + temp_mat.step[0] * row + temp_mat.step[1] * col);
+            if(col==0)
+                f1<<(int)*(temp_mat.data + temp_mat.step[0] * row + temp_mat.step[1] * col);
+            else
+                f1<<" "<<(int)*(temp_mat.data + temp_mat.step[0] * row + temp_mat.step[1] * col);
         }
+        f1<<"\n";
     }
-    
-
-
-
     f1.close();
+    //finish output person identify
+    
     return 0;
 }
+
+
 
